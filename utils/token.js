668209f112abb9665d0495a59a40702fe08e95f3
Aditya Das <93377048+AdityaDas31@users.noreply.github.com>
2025-07-17 21:16:49 +0530

    Update token.js

--- utils/token.js
+++ utils/token.js
@@ -1,11 +1,13 @@
-const sendToken = (user, statusCode, res) =>{
+const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
 
+    const isProduction = process.env.NODE_ENV === "production";
+
     const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
+        secure: isProduction,                    // Required for cross-site cookies
+        sameSite: isProduction ? "None" : "Lax"  // Allow frontend on different domain
     };
 
     res.status(statusCode).cookie("token", token, options).json({
@@ -14,5 +16,3 @@ const sendToken = (user, statusCode, res) =>{
         token,
     });
 };
-
-module.exports = sendToken;
